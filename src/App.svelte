2e78f1ab9e4f1b16c52418b457a43135c1d48571
Eirik Vageskar <eirikvageskar@gmail.com>
2020-02-15 14:19:00 +0100

    :construction: Add apple

--- src/App.svelte
+++ src/App.svelte
@@ -7,20 +7,39 @@
     { x: 2, y: 2 }
   ]
 
+  let apple_position = { x: 1, y: 1 }
+
   let board_dimensions = { x: 5, y: 5 }
 </script>
 
 <style>
-  .body-part {
+  .body-part,
+  .apple {
     position: absolute;
-    background-color: green;
     width: 60px;
     height: 60px;
+    text-align: center;
+  }
+
+  .body-part {
+    background-color: green;
+  }
+
+  .apple {
+    font-size: 48px;
   }
 </style>
 
-{#each snake_body as body_part}
+<div>
+  {#each snake_body as body_part}
+    <div
+      class="body-part"
+      style="left: {body_part.x * CELL_SIZE}px; top: {body_part.y * CELL_SIZE}px" />
+  {/each}
+
   <div
-    class="body-part"
-    style="left: {body_part.x * CELL_SIZE}px; top: {body_part.y * CELL_SIZE}px" />
-{/each}
+    style="left: {apple_position.x * CELL_SIZE}px; top: {apple_position.y * CELL_SIZE}px"
+    class="apple">
+    🍎
+  </div>
+</div>
