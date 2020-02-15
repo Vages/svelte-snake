4ca740f5925c59c5cd679b77b50be2f9f43b1bb5
Eirik Vageskar <eirikvageskar@gmail.com>
2020-02-15 14:10:26 +0100

    :construction: Render body to screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,26 @@
 <script>
-  let name = "world"
+  const CELL_SIZE = 60
 
-  let snake_body = []
+  let snake_body = [
+    { x: 4, y: 4 },
+    { x: 3, y: 3 },
+    { x: 2, y: 2 }
+  ]
 
   let board_dimensions = { x: 5, y: 5 }
 </script>
 
-<h1>Hello {name}!</h1>
+<style>
+  .body-part {
+    position: absolute;
+    background-color: green;
+    width: 60px;
+    height: 60px;
+  }
+</style>
+
+{#each snake_body as body_part}
+  <div
+    class="body-part"
+    style="left: {body_part.x * CELL_SIZE}px; top: {body_part.y * CELL_SIZE}px" />
+{/each}
