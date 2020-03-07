9a57c79d6a47757e7c35a01514b34c933d63924f
Eirik Vageskar <eirikvageskar@gmail.com>
2020-03-07 17:01:20 +0100

    :lipstick: Style modal

--- src/GameOverModal.svelte
+++ src/GameOverModal.svelte
@@ -23,16 +23,22 @@
     background-color: white;
     padding: 3rem;
   }
+
+  section {
+    margin: 2rem 0;
+  }
 </style>
 
-<div class="modal nes-container with-title is-rounded">
+<div class="modal nes-container with-title is-centered is-rounded">
   <h2 class="title">Game over</h2>
-  <h3>High scores</h3>
-  {#await getPromise}
-    <p>Fetching scores</p>
-  {:then highScores}
-    <div>
-      <div class="nes-table-responsive">
+  <section>
+    <h3>High scores</h3>
+    {#await getPromise}
+      <p>Fetching scores</p>
+    {:then highScores}
+      <div
+        class="nes-table-responsive"
+        style="width: max-content; margin-left: auto; margin-right: auto;">
         <table class="nes-table is-bordered is-centered">
           <thead>
             <tr>
@@ -50,36 +56,38 @@
           </tbody>
         </table>
       </div>
-    </div>
-  {:catch error}
-    <span class="nes-text is-error">Got error "{error.message}"</span>
-    <button class="nes-btn" on:click={fetchScores}>Retry</button>
-  {/await}
-  <h3>Submit score</h3>
-  {#if !postPromise}
-    <div>You got {score} points.</div>
-    <form on:submit={postScore}>
-      <div>
-        <label class="nes-field">
-          Name
-          <input
-            minlength="3"
-            required
-            type="text"
-            class="nes-input"
-            bind:value={name} />
-        </label>
-      </div>
-      <button type="submit" class="nes-btn">Submit</button>
-    </form>
-  {:else}
-    {#await postPromise}
-      <p>Submitting</p>
-    {:then success}
-      <div class="nes-text is-success">Successfully submitted!</div>
     {:catch error}
-      <span class="nes-text is-error">Got error "{error.message}"</span>
-      <button class="nes-btn" on:click={postScore}>Retry</button>
+      <div class="nes-text is-error">Got error "{error.message}"</div>
+      <button class="nes-btn" on:click={fetchScores}>Retry</button>
     {/await}
-  {/if}
+  </section>
+  <section>
+    <h3>Submit score</h3>
+    {#if !postPromise}
+      <div>You got {score} points.</div>
+      <form on:submit={postScore}>
+        <div>
+          <label class="nes-field">
+            Name
+            <input
+              minlength="3"
+              required
+              type="text"
+              class="nes-input"
+              bind:value={name} />
+          </label>
+        </div>
+        <button type="submit" class="nes-btn">Submit</button>
+      </form>
+    {:else}
+      {#await postPromise}
+        <p>Submitting</p>
+      {:then success}
+        <div class="nes-text is-success">Successfully submitted!</div>
+      {:catch error}
+        <div class="nes-text is-error">Got error "{error.message}"</div>
+        <button class="nes-btn" on:click={postScore}>Retry</button>
+      {/await}
+    {/if}
+  </section>
 </div>
